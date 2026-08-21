4965381e215339ee5d09f706251ff826e4ccb091
katsukikuroda <katsukikuroda@gmail.com>
2026-08-21 13:26:50 +0900

    2ndcmt

--- been_shot/config/settings.py
+++ been_shot/config/settings.py
@@ -130,6 +130,8 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 MEDIA_ROOT = BASE_DIR / "media"
 
+MEDIA_URL = "/media/" # 追加
+
 AUTH_USER_MODEL = "accounts.User"
 
 REST_FRAMEWORK = {
--- been_shot/config/urls.py
+++ been_shot/config/urls.py
@@ -1,3 +1,5 @@
+from django.conf import settings
+from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import include, path
 
@@ -6,3 +8,4 @@ urlpatterns = [
     path('api/auth/', include('accounts.urls')),
     path("api/", include("posts.urls")),
 ]
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
